3647545d87d379cc2263453ebacdeebf5d7afdb3
JoeyFTribbiani <vincent@VincentdeMacBook-Pro.local>
2015-05-07 14:18:13 +0800

    7

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,10 @@
 var app = require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
+AV.Cloud.define('hello', function(request, response) {
+    response.success('Hello world!');
+});
+
 AV.Cloud.define("generateAddressAuthenticationCode",function(req, res) {
 	var address = req.params.address;
 	var neighbourhood;
