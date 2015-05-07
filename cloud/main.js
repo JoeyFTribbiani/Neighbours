4f322234fbea2364f98450f876d8238e415a768c
JoeyFTribbiani <vincent@VincentdeMBP.Home>
2015-05-07 20:47:34 +0800

    qw

--- cloud/main.js
+++ cloud/main.js
@@ -19,7 +19,7 @@ AV.Cloud.define('hello', function(request, response) {
 
 AV.Cloud.define("getPhoneAuthenticationCode",function(req, res) {
     var phone = req.params.phone
-    AV.Query.doCloudQuery("select count(*) from PhoneAuthenticationCode where phone=? and createdAt<?",[phone,moment().endOf('day')],{
+    AV.Query.doCloudQuery("select count(*) from PhoneAuthenticationCode where phone='"+phone+"' and createdAt<date("+moment().endOf('day')+")",{
         success: function(result){
             if(result.count > 3){
                 res.success({
