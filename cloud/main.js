ce5191148838b188a1769a7660e0c6db4d01b5b8
JoeyFTribbiani <vincent@VincentdeMBP.Home>
2015-05-08 08:29:32 +0800

    updated

--- cloud/main.js
+++ cloud/main.js
@@ -17,56 +17,56 @@ AV.Cloud.define('hello', function(request, response) {
     response.success('Hello world!');
 });
 
-//AV.Cloud.define("register",function(req, res) {
-//    var phone = req.params.phone
-//    var code = req.params.code
-//    var gender = req.params.gender
-//    var birthday = req.params.birthday
-//    var nickname = req.params.nickname
-//    var password = req.params.password
-//    if(verifyPhone(phone,code)){
-//        var user = new AV.User();
-//        user.set("username", phone);
-//        user.set("password", password);
-//        user.set("birthday", birthday);
-//        user.set("nickname", nickname);
-//        user.set("gender", gender);
-//        user.signUp(null, {
-//            success: function(user) {
-//                // Hooray! Let them use the app now.
-//                user.save({
-//                    "mobilePhoneNumber":phone
-//                }, {
-//                    success: function(user) {
-//                        // The save was successful.
-//                        res.success(user)
-//                    },
-//                    error: function(user, error) {
-//                        // The save failed.  Error is an instance of AV.Error.
-//                        res.error(error)
-//                    }
-//                })
-//            },
-//            error: function(user, error) {
-//                // Show the error message somewhere and let the user try again.
-//                res.error("用户已注册")
-//            }
-//        });
-//    }else{
-//        res.error("验证码错误")
-//    }
-//});
-//
-//function verifyPhone(phone,code){
-//    AV.Cloud.verifySmsCode(phone,code,{
-//        success:function(){
-//            return true
-//        },
-//        error:function(){
-//            return false
-//        }
-//    })
-//};
+AV.Cloud.define("register",function(req, res) {
+    var phone = req.params.phone
+    var code = req.params.code
+    var gender = req.params.gender
+    var birthday = req.params.birthday
+    var nickname = req.params.nickname
+    var password = req.params.password
+    if(verifyPhone(phone,code)){
+        var user = new AV.User();
+        user.set("username", phone);
+        user.set("password", password);
+        user.set("birthday", birthday);
+        user.set("nickname", nickname);
+        user.set("gender", gender);
+        user.signUp(null, {
+            success: function(user) {
+                // Hooray! Let them use the app now.
+                user.save({
+                    "mobilePhoneNumber":phone
+                }, {
+                    success: function(user) {
+                        // The save was successful.
+                        res.success(user)
+                    },
+                    error: function(user, error) {
+                        // The save failed.  Error is an instance of AV.Error.
+                        res.error(error)
+                    }
+                })
+            },
+            error: function(user, error) {
+                // Show the error message somewhere and let the user try again.
+                res.error("用户已注册")
+            }
+        });
+    }else{
+        res.error("验证码错误")
+    }
+});
+
+function verifyPhone(phone,code){
+    AV.Cloud.verifySmsCode(phone,code,{
+        success:function(){
+            return true
+        },
+        error:function(){
+            return false
+        }
+    })
+};
 
 
 AV.Cloud.define("getPhoneAuthenticationCode",function(req, res) {
@@ -76,7 +76,7 @@ AV.Cloud.define("getPhoneAuthenticationCode",function(req, res) {
             if(result.count > 2){
                 res.error("申请次数达到上限")
             }else{
-                AV.User.requestMobilePhoneVerify(phone).then(function(){
+                AV.Cloud.requestMobilePhoneVerify(phone).then(function(){
                     var phoneAuthenticationCode = new PhoneAuthenticationCode()
                     phoneAuthenticationCode.save({
                        "phone": phone
